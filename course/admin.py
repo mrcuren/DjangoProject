75c6cc319de579bf25fedc53f3652d4ec02a6b36
mrcuren <101352238+mrcuren@users.noreply.github.com>
2022-11-23 00:30:32 +0300

    kategoriler

--- course/admin.py
+++ course/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-
+from mptt.admin import MPTTModelAdmin
 from course.models import Category,Course,Images
+from mptt.admin import DraggableMPTTAdmin
+from .models import Category, Course
 # Register your models here.
 class CourseImageInline(admin.TabularInline):
     model =Images
@@ -19,6 +21,40 @@ class CourseAdmin(admin.ModelAdmin):
 class ImagesAdmin(admin.ModelAdmin):
     list_display = ['title',"course","image"]
 
-admin.site.register(Category,CategoryAdmin)
+
+class CategoryAdmin2(DraggableMPTTAdmin):
+    mptt_indent_field = "title"
+    list_display = ('tree_actions', 'indented_title',
+                    'related_products_count', 'related_products_cumulative_count')
+    list_display_links = ('indented_title',)
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+
+        # Add cumulative product count
+        qs = Category.objects.add_related_count(
+                qs,
+                Course,
+                'category',
+                'products_cumulative_count',
+                cumulative=True)
+
+        # Add non cumulative product count
+        qs = Category.objects.add_related_count(qs,
+                 Course,
+                 'category',
+                 'products_count',
+                 cumulative=False)
+        return qs
+
+    def related_products_count(self, instance):
+        return instance.products_count
+    related_products_count.short_description = 'Related products (for this specific category)'
+
+    def related_products_cumulative_count(self, instance):
+        return instance.products_cumulative_count
+    related_products_cumulative_count.short_description = 'Related products (in tree)'
+
+admin.site.register(Category,CategoryAdmin2)
 admin.site.register(Course,CourseAdmin)
 admin.site.register(Images,ImagesAdmin)
--- home/admin.py
+++ home/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 
-from home.models import Setting
-
+from home.models import Setting,ContactFormMessage
+class ContactFormMessageAdmin(admin.ModelAdmin):
+    list_display = ['name', 'email','subject','message','note','status']
+    list_filter = ["status"]
 # Register your models here.
+
+admin.site.register(ContactFormMessage,ContactFormMessageAdmin)
 admin.site.register(Setting)
