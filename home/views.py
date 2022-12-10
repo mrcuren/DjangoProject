dd24d5d70750f7ad5a1801bb7fcbfc073b5f7641
mrcuren <101352238+mrcuren@users.noreply.github.com>
2022-12-10 23:29:10 +0300

    10.12

--- home/views.py
+++ home/views.py
@@ -1,13 +1,16 @@
+from django.contrib import messages
 from django.shortcuts import render
-from django.http import HttpResponse
-
-from home.models import Setting
+from django.http import HttpResponse,HttpResponseRedirect
+from home.models import Setting,ContactFormMessage,ContactFormu
+from course.models import Category
 
 
 # Create your views here.
 def index(request):
+
     setting = Setting.objects.get(pk=1)
-    context = {'setting': setting,'page':'home'}
+    category = Category.objects.all()
+    context = {'setting': setting, 'page':'home','category': category}
     return render(request, 'index.html', context)
 
 def hakkimizda(request):
@@ -15,3 +18,27 @@ def hakkimizda(request):
     context = {'setting': setting,'page':'hakkimizda'}
     return render(request, 'hakkimizda.html', context)
 
+def referanslar(request):
+    setting = Setting.objects.get(pk=1)
+    context = {'setting': setting,'page':'referanslarimiz'}
+    return render(request, 'referanslarimiz.html', context)
+
+
+
+def iletisim(request):
+    if request.method == 'POST':
+        form= ContactFormu(request.POST)
+        if form.is_valid():
+            data=ContactFormMessage()
+            data.name=form.cleaned_data['name']
+            data.email=form.cleaned_data['email']
+            data.subject = form.cleaned_data['subject']
+            data.message=form.cleaned_data['message']
+            data.ip= request.META.get('REMOTE_ADDR')
+            data.save()
+            messages.success(request,"Mesajınız başarı ile gönderilmiştir.Teşekkür ederiz")
+            return HttpResponseRedirect ('/iletisim')
+    setting = Setting.objects.get(pk=1)
+    form=ContactFormu()
+    context = {'setting': setting,'form': form}
+    return render(request, 'iletisim.html', context)
